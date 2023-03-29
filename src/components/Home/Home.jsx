05cabb79974d44b71e052cf16fdfb458f81e61de
LOCO185 <jloco151@gmail.com>
2023-03-29 10:21:19 +0300

    Added some styles

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,26 +1,43 @@
-import React from "react";
+// import React from "react";
+import React, { useState } from "react";
+import { useQuill } from "react-quilljs";
+import "quill/dist/quill.snow.css";
 import Navbar from "../Navbar/Navbar";
 import "../Home/Home.css";
-import HtmlEditor from "../HtmlEditor/HtmlEditor";
+// import HtmlEditor from "../HtmlEditor/HtmlEditor";
 import Footer from "../Footer/Footer";
 
 const Home = () => {
+  const { quill, quillRef } = useQuill();
+  const [value, setValue] = useState();
+
+  React.useEffect(() => {
+    if (quill) {
+      quill.on("text-change", () => {
+        console.log(quillRef.current.firstChild.innerHTML);
+        setValue(quillRef.current.firstChild.innerHTML);
+      });
+    }
+  }, [quill, quillRef]);
+
   return (
     <div>
       <Navbar />
       <h1>Home page</h1>
-      <div className="container">
-        <HtmlEditor />
-        <div className="boxes">
-          <div className="first-row">
-            <div className="div1"></div>
-            <div className="div2"></div>
-          </div>
-          <div className="second-row">
-            <div className="div3"></div>
-            <div className="div4"></div>
-          </div>
+      <div className="main">
+        {/* <HtmlEditor /> */}
+        <div
+          className="editor"
+          style={{
+            margin: "auto",
+            maxWidth: 800,
+            height: 500,
+            backgroundColor: "white",
+          }}
+        >
+          <div ref={quillRef} />
         </div>
+        <div className="boxes"></div>
       </div>
       <button className="btn"> Submit </button>
       <Footer />
