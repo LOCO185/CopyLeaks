164e14af34706778b50d1c3dc506575572b2fbc4
LOCO185 <jloco151@gmail.com>
2023-04-02 13:57:54 +0300

    Testing with postman

--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+  const user = req.query.user;
+
+  res.send(user + "!");
+});
+
 app.listen(5000, () => {
   console.log("server started on port 5000");
 });
